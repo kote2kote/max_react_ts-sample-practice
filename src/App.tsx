ec8892499f5ff78857899fdf49ca285f773ddb01
kote2 <kote2kote2@gmail.com>
2022-02-10 17:14:34 +0900

    starter

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,3 @@
-// import "./styles.css";
-
-// export default function App() {
-//   return (
-//     <div className="App">
-//       <h1>Hello CodeSandbox</h1>
-//       <h2>Start editing to see some magic happen!</h2>
-//     </div>
-//   );
-// }
-
-// import NewTodo from "./components/NewTodo";
-// import Todos from "./components/Todos";
-// import TodosContextProvider from "./store/todos-context";
-
-// function App() {
-//   return (
-//     <TodosContextProvider>
-//       <NewTodo />
-//       <Todos />
-//     </TodosContextProvider>
-//   );
-// }
-
 import Todos from "./components/Todos";
 function App() {
   return (
